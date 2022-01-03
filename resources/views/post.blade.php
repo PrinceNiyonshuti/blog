a5b2fe2e1bf19d93217ba0d16e5d08a0eb5bb3a4
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-03 18:48:06 +0200

    Make a Route and Link to it page

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post - Single</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta unde perspiciatis 
+            eos dolorum nisi ipsum eaque. Exercitationem a odio blanditiis magni, molestias 
+            dolores distinctio repellendus quas repellat id? Tempora, pariatur ,Lorem ipsum 
+            dolor sit, amet consectetur adipisicing elit. Ab, voluptatem nemo obcaecati 
+            reprehenderit commodi corporis fuga rem excepturi, vitae magni illum, reiciendis 
+            nostrum asperiores officiis suscipit animi similique! Ullam, natus?
+        </p>
+    </article>
+    <a href="/">Back Home</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+Route::get('/post',function(){
+    return view('post');
 });
