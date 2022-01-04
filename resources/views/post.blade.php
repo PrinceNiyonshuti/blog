961cece3166420df953dc3477de9ba2b4784e9cf
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-04 21:15:21 +0200

    template inheritance

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post - Single</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
+
     <article>
         <h1>{{ $post->title }}</h1>
         <div>
@@ -15,5 +9,5 @@
         </div>
     </article>
     <a href="/">Back Home</a>
-</body>
-</html>
+
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
+@extends('layout')
+
+@section('banner')
+    <h1>My Blogs</h1>
+@endsection
+
+@section('content')
+
     @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'yes':'' }}">
             <h1> <a href="/posts/{{ $post->slug }}">{{$post->title}}</a> </h1>
@@ -17,6 +14,5 @@
             </div>
         </article>
     @endforeach
-    <article>
-</body>
-</html>
+
+@endsection
