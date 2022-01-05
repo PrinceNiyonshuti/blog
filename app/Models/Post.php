826f9a50fe88890adf89ad6707aec0e76adee76f
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-05 09:57:33 +0200

    3 Ways to Mitigate Mass Assignment Vulnerabilities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // 3 ways to allow mass assignment and its vulnerable protection
+
+    protected $fillable = ['title','excerpt','body',];
+
+    // protected $guard =['id'];
+
+    // protected $guard =[];
 }
