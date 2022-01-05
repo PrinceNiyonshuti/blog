51a1ff4f6c1c15fb56befd6dc713974463cc80d9
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-05 13:11:08 +0200

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,62 +21,18 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
         Category::truncate();
 
+        // Altenative way after creating each factory for model
+        // Post::factory()->create();
 
-        // seed the tables
-        $user = User::factory(1)->create();
-
-        $personal = Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
-        ]);
-        $work = Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-        $family = Category::create([
-            'name'=>'Family',
-            'slug'=>'family'
+        $user = User::factory()->create([
+            'name'=>'John Doe',
         ]);
 
-        Post::create([
-            'user_id'=> 1,
-            'category_id'=>$family->id,
-            'title'=>'My First post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
-            'body'=>'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
-        ]);
-        Post::create([
-            'user_id'=> 1,
-            'category_id'=>$work->id,
-            'title'=>'My second post',
-            'slug'=>'my-second-post',
-            'excerpt'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
-            'body'=>'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
-        ]);
-        Post::create([
-            'user_id'=> 1,
-            'category_id'=>$personal->id,
-            'title'=>'My third post',
-            'slug'=>'my-third-post',
-            'excerpt'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
-            'body'=>'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
-        ]);
-        Post::create([
-            'user_id'=> 1,
-            'category_id'=>$personal->id,
-            'title'=>'My fourth post',
-            'slug'=>'my-fourth-post',
-            'excerpt'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
-            'body'=>'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
-        ]);
-        Post::create([
-            'user_id'=> 1,
-            'category_id'=>$work->id,
-            'title'=>'My fifth post',
-            'slug'=>'my-fifth-post',
-            'excerpt'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
-            'body'=>'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.'
-        ]);
+        Post::factory(5)->create(
+            [
+                'user_id'=> $user->id,
+            ]
+        );
+
     }
 }
