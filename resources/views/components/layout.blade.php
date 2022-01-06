b9aa2bddc91542fe93c3f07c31889b2cb699e1d9
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-06 20:55:57 +0200

    A Small JavaScript Dropdown Detour

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@2.8.2/dist/alpine.js" defer></script>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('posts',[
-        'posts'=>Post::latest('id')->get()
+        'posts'=>Post::latest('id')->get(),
+        'categories'=>Category::all()
     ]);
 });
 
@@ -32,12 +33,15 @@ Route::get('/posts/{post:slug}',function(Post $post){
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
-        'posts'=> $category->posts
+        'posts'=> $category->posts,
+        'currentCategory'=>$category,
+        'categories'=>Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}',function(User $author){
     return view('posts',[
-        'posts'=> $author->posts
+        'posts'=> $author->posts,
+        'categories'=>Category::all()
     ]);
 });
