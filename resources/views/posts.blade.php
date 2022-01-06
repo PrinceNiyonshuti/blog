ef81c9a77fcac6d956dd9dd11a2d147944ff46f9
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-06 18:45:07 +0200

    Convert the HTML and CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -18,7 +18,7 @@
 @endsection --}}
 
 
-<x-layout>
+{{-- <x-layout>
     @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'yes':'' }}">
             <h1>
@@ -35,4 +35,22 @@
             </div>
         </article>
     @endforeach
+</x-layout> --}}
+<x-layout>
+        @include('_post-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card/>
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+        </main>
 </x-layout>
