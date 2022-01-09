81ad604240be6b903ab343f97d2b243f7f5e9824
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-09 18:37:55 +0200

    Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex border border-gray-200 bg-gray-100 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="user" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posterd <time>3 weeks ago</time> </p>
+        </header>
+        <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+            cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper
+            sit amet ligula. Vivamus suscipit tortor eget felis porttitor volutpat.
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,6 +51,12 @@
                     </p>
                 </div>
             </div>
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-post-comment/>
+                <x-post-comment/>
+                <x-post-comment/>
+                <x-post-comment/>
+            </section>
         </article>
     </main>
 </x-layout>
