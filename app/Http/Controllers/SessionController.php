9a4f2783b8ec029041f11566ff7c2b5083808a37
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-09 17:34:53 +0200

    login page and logout handled

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,35 @@ class SessionController extends Controller
     public function destroy()
     {
         auth()->logout();
-        return \redirect('/')->with('success' , 'See you soon !');
+        return redirect('/')->with('success' , 'See you soon !');
+    }
+
+    public function create()
+    {
+        return view('session.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attibutes = request()->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required'
+        ]);
+
+        // attempt to authenticate and log in the user
+        if(auth()->attempt($attibutes)){
+
+            // create new session
+            session()->regenerate();
+
+            // redirect to home page
+            return redirect('/')->with('success','welcome back !');
+        }
+
+        // auth failed
+        return back()
+        ->withInput()
+        ->withErrors(['password'=> 'Password could not be verified']);
     }
 }
---
+++ resources/views/session/create.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border-gray-200 p-6 rounded-xl">
+            <h1 class="text-center font-bold text-xl">Login</h1>
+            <form method="POST" action="/login">
+                @csrf
+
+                <div class="mb-6">
+                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        email
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" value="{{ old('email') }}" required>
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Password
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <button type="submit" class=" bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Login</button>
+                </div>
+            </form>
+        </main>
+    </section>
+
+
+</x-layout>
